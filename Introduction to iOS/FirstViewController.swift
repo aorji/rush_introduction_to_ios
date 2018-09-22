bd7ec5125202d441578072532c0170e565d8b83a
itsmekate <prasolkaterina@knu.ua>
2018-09-22 15:25:54 +0300

    aeg control and annotation

--- Introduction to iOS/FirstViewController.swift
+++ Introduction to iOS/FirstViewController.swift
@@ -13,15 +13,15 @@ class Uf: NSObject, MKAnnotation {
     let title: String?
     let locationName: String
     let coordinate: CLLocationCoordinate2D
-    
+
     init(title: String, locationName: String, coordinate: CLLocationCoordinate2D) {
         self.title = title
         self.locationName = locationName
         self.coordinate = coordinate
-        
+
         super.init()
     }
-    
+
     var subtitle: String? {
         return locationName
     }
@@ -29,18 +29,33 @@ class Uf: NSObject, MKAnnotation {
 
 class FirstViewController: UIViewController {
 
+    @IBAction func segCtrlAction(_ sender: Any) {
+        switch ((sender as AnyObject).selectedSegmentIndex) {
+        case 0:
+            mapView.mapType = .standard
+        case 1:
+            mapView.mapType = .satellite
+        default:
+            mapView.mapType = .hybrid
+        }
+    }
+
     @IBOutlet weak var mapView: MKMapView!
+    
+    let uf = Uf(title: "UNIT Factory",
+                locationName: "Educational institution",
+                coordinate: CLLocationCoordinate2D(latitude: 50.469713, longitude: 30.462223))
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 		let initialLocation = CLLocation(latitude: 50.469713, longitude: 30.462223)
+        mapView.delegate = self
 		centerMapOnLocation(location: initialLocation)
         mapView.addAnnotation(uf)
-        // Do any additional setup after loading the view, typically from a nib.
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
 	
     let regionRadius: CLLocationDistance = 1000
@@ -48,11 +63,25 @@ class FirstViewController: UIViewController {
         let coordinateRegion = MKCoordinateRegionMakeWithDistance(location.coordinate, regionRadius, regionRadius)
         mapView.setRegion(coordinateRegion, animated: true)
     }
-    
-    let uf = Uf(title: "UNIT Factory",
-                locationName: "Educational institution",
-                coordinate: CLLocationCoordinate2D(latitude: 50.469713, longitude: 30.462223))
 
 }
 
+extension FirstViewController: MKMapViewDelegate {
+    func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
+        guard let annotation = annotation as? Uf else { return nil }
+        let identifier = "marker"
+        var view: MKMarkerAnnotationView
+        if let dequeuedView = mapView.dequeueReusableAnnotationView(withIdentifier: identifier)
+            as? MKMarkerAnnotationView {
+            dequeuedView.annotation = annotation
+            view = dequeuedView
+        } else {
+            view = MKMarkerAnnotationView(annotation: annotation, reuseIdentifier: identifier)
+            view.canShowCallout = true
+            view.calloutOffset = CGPoint(x: -5, y: 5)
+            view.rightCalloutAccessoryView = UIButton(type: .detailDisclosure)
+        }
+        return view
+    }
+}
 

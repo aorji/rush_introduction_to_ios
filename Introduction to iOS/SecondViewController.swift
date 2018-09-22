e00c5e03a74003484ef303e459e33ba30818e98a
Alina FESYK <afesyk@e1r2p4.unit.ua>
2018-09-22 13:51:13 +0300

    missed files

--- Introduction to iOS/SecondViewController.swift
+++ Introduction to iOS/SecondViewController.swift
@@ -8,18 +8,39 @@
 
 import UIKit
 
-class SecondViewController: UIViewController {
+class SecondViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
 
-    override func viewDidLoad() {
+	@IBOutlet weak var tableView: UITableView!
+	var barIndexDictionary : [String: Int] = [:]
+	
+	override func viewDidLoad() {
         super.viewDidLoad()
+		
         // Do any additional setup after loading the view, typically from a nib.
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
+		tableView.reloadData()
         // Dispose of any resources that can be recreated.
     }
 
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 0
+    }
 
+	func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+		return UITableViewCell()
+	}
+	
+	func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+		
+	}
+	
+	func addMemberToDictionary() -> [String: Int] {
+		for index in 0...2 {
+			
+		}
+	}
 }
 

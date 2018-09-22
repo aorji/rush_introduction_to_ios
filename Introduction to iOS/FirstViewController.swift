ab8aaee9012063d41eba55be07ba63201d16f52a
itsmekate <prasolkaterina@knu.ua>
2018-09-22 14:23:46 +0300

    added map, Uf class and small annotation

--- Introduction to iOS/FirstViewController.swift
+++ Introduction to iOS/FirstViewController.swift
@@ -7,11 +7,34 @@
 //
 
 import UIKit
+import MapKit
+
+class Uf: NSObject, MKAnnotation {
+    let title: String?
+    let locationName: String
+    let coordinate: CLLocationCoordinate2D
+    
+    init(title: String, locationName: String, coordinate: CLLocationCoordinate2D) {
+        self.title = title
+        self.locationName = locationName
+        self.coordinate = coordinate
+        
+        super.init()
+    }
+    
+    var subtitle: String? {
+        return locationName
+    }
+}
 
 class FirstViewController: UIViewController {
 
+    @IBOutlet weak var mapView: MKMapView!
     override func viewDidLoad() {
         super.viewDidLoad()
+		let initialLocation = CLLocation(latitude: 50.469713, longitude: 30.462223)
+		centerMapOnLocation(location: initialLocation)
+        mapView.addAnnotation(uf)
         // Do any additional setup after loading the view, typically from a nib.
     }
 
@@ -19,7 +42,17 @@ class FirstViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+	
+    let regionRadius: CLLocationDistance = 1000
+    func centerMapOnLocation(location: CLLocation) {
+        let coordinateRegion = MKCoordinateRegionMakeWithDistance(location.coordinate, regionRadius, regionRadius)
+        mapView.setRegion(coordinateRegion, animated: true)
+    }
+    
+    let uf = Uf(title: "UNIT Factory",
+                locationName: "Educational institution",
+                coordinate: CLLocationCoordinate2D(latitude: 50.469713, longitude: 30.462223))
 
 }
+
 

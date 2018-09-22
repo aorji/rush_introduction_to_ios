95b05311945f02a27b2494c364afa073787c0fe8
Alina FESYK <afesyk@e1r1p17.unit.ua>
2018-09-22 10:35:54 +0300

    Initial Commit

---
+++ Introduction to iOS/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  Introduction to iOS
+//
+//  Created by Alina FESYK on 9/22/18.
+//  Copyright © 2018 Alina FESYK. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Introduction to iOS/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  Introduction to iOS
+//
+//  Created by Alina FESYK on 9/22/18.
+//  Copyright © 2018 Alina FESYK. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
